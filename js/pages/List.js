2cea66ae7493114ffad48fd546aeb323f4132a19
Emonadeo <emonadeo@gmail.com>
2022-04-06 01:42:47 +0200

    Added Meta section

--- js/pages/List.js
+++ js/pages/List.js
@@ -67,6 +67,67 @@ export default {
                     </table>
                 </div>
             </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <h3>List Editors</h3>
+                    <ul class="editors">
+                        <li>
+                            <img src="/assets/crown.svg" alt="Owner">
+                            <p>IRaily</p>
+                        </li>
+                        <li>
+                            <img src="/assets/user-gear.svg" alt="Helper">
+                            <p>AcropolisBoy</p>
+                        </li>
+                        <li>
+                            <img src="/assets/user-gear.svg" alt="Helper">
+                            <p>SupaX_</p>
+                        </li>
+                        <li>
+                            <img src="/assets/user-gear.svg" alt="Helper">
+                            <p>ExtremeBeemon</p>
+                        </li>
+                        <li>
+                            <img src="/assets/user-gear.svg" alt="Helper">
+                            <p>hex</p>
+                        </li>
+                        <li>
+                            <img src="/assets/user-gear.svg" alt="Helper">
+                            <p>Davine_007</p>
+                        </li>
+                        <li>
+                            <img src="/assets/code.svg" alt="Developer">
+                            <p>Electro</p>
+                        </li>
+                        <li>
+                            <img src="/assets/code.svg" alt="Developer">
+                            <p>Prometheus</p>
+                        </li>
+                    </ul>
+                    <h3>Submission Requirements</h3>
+                    <p>
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                    </p>
+                    <p>
+                        Do not use secret routes or bug routes
+                    </p>
+                    <p>
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
+                </div>
+            </div>
         </main>
     `,
     data: () => ({
