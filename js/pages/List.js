fbdf60f6a6160811155b038af4c9cf1810057ba7
xAcidius <73004495+xAcidius@users.noreply.github.com>
2022-07-07 20:13:51 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -52,7 +52,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to copy' }}</p>
+                            <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
