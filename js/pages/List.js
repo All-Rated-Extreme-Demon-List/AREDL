5497449f7fb2012d73b1ce9ba7ae251b6fb1fcf1
SkeletalVirus <61529324+SkeletalVirus@users.noreply.github.com>
2022-10-28 12:54:08 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -88,7 +88,7 @@ export default {
                         <p class="type-label-md">Original List by <a href="https://me.redlimerl.com/" target="_blank">RedLime</a></p>
                     </div>
                     <template v-if="editors">
-                        <h3>CRGDPS Staff</h3>
+                        <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
                                 <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
