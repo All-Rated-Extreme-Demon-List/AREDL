5010616e66611a3931511f94eaefd9cb24758a1f
SkeletalVirus <61529324+SkeletalVirus@users.noreply.github.com>
2022-11-16 11:36:43 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -15,7 +15,7 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
-    let score = (100 / Math.sqrt((rank - 1) / 25 + 0.444444) - 25) *
+    let score = (100 / Math.sqrt((rank - 1) / 400 + 0.444444) - 400) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
