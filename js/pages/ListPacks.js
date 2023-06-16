5586be2042bafc66a11d90658494b6a6ec1eadef
KrisGra <39800337+krisgrant@users.noreply.github.com>
2023-06-16 19:03:37 +0300

    finish packs page layout

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -14,75 +14,180 @@ export default {
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
-        <main v-else class="page-packs">
-            <div class="packs-nav">
-                <button @click="switchLevels(i)" v-for="(pack, i) in packs" :style="{backgroundColor: pack.colour, color: getFontColour(pack.colour)}" class="type-label-lg">
-                    {{pack.name}}
-                </button>
+        <div class="packs-nav">
+            <button @click="switchLevels(i)" v-for="(pack, i) in packs" :style="{backgroundColor: pack.colour, color: getFontColour(pack.colour)}" class="type-label-lg">
+                {{pack.name}}
+            </button>
+        </div>
+        <main v-else class="pack-list">
+            <div class="list-container">
+                <table class="list" v-if="selectedPackLevels">
+                    <tr v-for="(level, i) in selectedPackLevels">
+                        <td class="rank">
+                            <p class="type-label-lg">#{{ i + 1 }}</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selectedLevel == i, 'error': !level }">
+                            <button :style= "[selectedLevel == i ? {backgroundColor: pack.colour, color: getFontColour(pack.colour)} : {}]" @click="selectedLevel = i">
+                                <span class="type-label-lg">{{ level[0].level.name || \`Error (\.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
             </div>
-            <div v-if="loadingPack">
-                <Spinner></Spinner>
-            </div>
-            <div v-else class="pack-list">
-                <div class="list-container">
-                    <table class="list">
-                        <tr v-for="(level, i) in selectedPackLevels">
-                            <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
+            <div class="level-container">
+                <div class="level" v-if="selectedPackLevels[selectedLevel]">
+                    <h1>{{ selectedPackLevels[selectedLevel][0].level.name }}</h1>
+                    <LevelAuthors :author="selectedPackLevels[selectedLevel][0].level.author" :creators="selectedPackLevels[selectedLevel][0].level.creators" :verifier="selectedPackLevels[selectedLevel][0].level.verifier"></LevelAuthors>
+                    <div style="display:flex">
+                        <div v-for="pack in selectedPackLevels[selectedLevel][0].level.packs" class="tag" :style="{background:pack.colour, color:getFontColour(pack.colour)}">{{pack.name}}</div>
+                    </div>
+                    <iframe class="video" :src="embed(selectedPackLevels[selectedLevel][0].level.verification)" frameborder="0"></iframe>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
+                            <p>{{ score(selected + 1, 100, selectedPackLevels[selectedLevel][0].level.percentToQualify) }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ selectedPackLevels[selectedLevel][0].level.id }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">Password</div>
+                            <p>{{ selectedPackLevels[selectedLevel][0].level.password || 'Free to Copy' }}</p>
+                        </li>
+                    </ul>
+                    <h2>Records</h2>
+                    <p v-if="selected + 1 <= 150"><strong>{{ selectedPackLevels[selectedLevel][0].level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else>100% or better to qualify</p>
+                    <table class="records">
+                        <tr v-for="record in selectedPackLevels[selectedLevel][0].level.records" class="record">
+                            <td class="percent">
+                                <p>{{ record.percent }}%</p>
                             </td>
-                            <td class="level" :class="{ 'active': selectedLevel == i, 'error': !level }">
-                                <button :style= "[selectedLevel == i ? {backgroundColor: pack.colour, color: getFontColour(pack.colour)} : {}]" @click="selectedLevel = i">
-                                    <span class="type-label-lg">{{ level[0].level.name || \`Error (\.json)\` }}</span>
-                                </button>
+                            <td class="user">
+                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                            </td>
+                            <td class="mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
+                            </td>
+                            <td class="hz">
+                                <p>{{ record.hz }}Hz</p>
                             </td>
                         </tr>
                     </table>
                 </div>
-                <div class="level-container">
-                    <div class="level" v-if="selectedPackLevels[selectedLevel]">
-                        <h1>{{ selectedPackLevels[selectedLevel][0].level.name }}</h1>
-                        <LevelAuthors :author="selectedPackLevels[selectedLevel][0].level.author" :creators="selectedPackLevels[selectedLevel][0].level.creators" :verifier="selectedPackLevels[selectedLevel][0].level.verifier"></LevelAuthors>
-                        <div style="display:flex">
-                            <div v-for="pack in selectedPackLevels[selectedLevel][0].level.packs" class="tag" :style="{background:pack.colour, color:getFontColour(pack.colour)}">{{pack.name}}</div>
-                        </div>
-                        <iframe class="video" :src="embed(selectedPackLevels[selectedLevel][0].level.verification)" frameborder="0"></iframe>
-                        <ul class="stats">
-                            <li>
-                                <div class="type-title-sm">Points when completed</div>
-                                <p>{{ score(selected + 1, 100, selectedPackLevels[selectedLevel][0].level.percentToQualify) }}</p>
-                            </li>
-                            <li>
-                                <div class="type-title-sm">ID</div>
-                                <p>{{ selectedPackLevels[selectedLevel][0].level.id }}</p>
-                            </li>
-                            <li>
-                                <div class="type-title-sm">Password</div>
-                                <p>{{ selectedPackLevels[selectedLevel][0].level.password || 'Free to Copy' }}</p>
-                            </li>
-                        </ul>
-                        <h2>Records</h2>
-                        <p v-if="selected + 1 <= 150"><strong>{{ selectedPackLevels[selectedLevel][0].level.percentToQualify }}%</strong> or better to qualify</p>
-                        <p v-else>100% or better to qualify</p>
-                        <table class="records">
-                            <tr v-for="record in selectedPackLevels[selectedLevel][0].level.records" class="record">
-                                <td class="percent">
-                                    <p>{{ record.percent }}%</p>
-                                </td>
-                                <td class="user">
-                                    <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
-                                </td>
-                                <td class="mobile">
-                                    <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
-                                </td>
-                                <td class="hz">
-                                    <p>{{ record.hz }}Hz</p>
-                                </td>
-                            </tr>
-                        </table>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                </div>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
-                    <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
-                        <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                    <div class="og">
+                        <p class="type-label-md">Original List by <a href="https://tsl.pages.dev/#/" target="_blank">TheShittyList</a></p>
                     </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <p>
+                        WARNING: Records will take a while after the <a href="https://www.youtube.com/watch?v=Pr5uMhDn_8U" target="_blank">AeonAir Video</a>, please refer to the information below
+                    </p>
+                    <h3>How to Submit Records</h3>
+                    <p>
+                        Join the discord, and  use /record submit
+                    </p>
+                    <p>
+                        If your records hasn't accepted or denied after 1 week of submitting it, please resubmit the record again
+                    </p>
+                    <h3>Why was my record denied?</h3>
+                    <p>
+                        If you're record was denied, please check the following
+                    </p>
+                    <p>
+                        Does the video follow the requirements? (Below)
+                    </p>
+                    <p>
+                        Is the level placed on the list? (#pending-placements)
+                    </p>
+                    <p>
+                        Was the submission command filled out correctly?
+                    </p>
+                    <p>
+                        Was the record submitted with several links?
+                    </p>
+                    <p>
+                        Note: The record will say its been denied and the reason in #records, if it isn't accepted or denied after a week, the record will need to resubmitted
+                    </p>
+                    <p>
+                        If the record was wrongfully denied, please make a post in #support or DM SkeletalVirus#9604 on Discord
+                    </p>
+                    <h3>Submission Requirements</h3>
+                    <p>
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                    </p>
+                    <p>
+                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Do not use secret routes or bug routes
+                    </p>
+                    <p>
+                        Do not use Banned Hacks (<a href="https://docs.google.com/document/d/1AsQS0WH_OzPAzS7avtAB4e8rnYhoqWnFsV068Tcu5VU/edit?usp=sharing" target="_blank">Full List of Allowed / Banned hacks listed here</a>)
+                    </p>
+                     <p>
+                       <h3>Allowed Hacks / Modifications</h3>
+                    </p>
+                    <p>
+                       - LDMs / ULDMs
+                    </p>
+                    <p>
+                       - Show Hitboxes on Death
+                    </p>
+                    <p>
+                       - Cosmetic Hacks (Show / Hide Player Trail, RGB Icons, etc)
+                    </p>
+                    <p>
+                       - Speedhack above x1 Speed
+                    </p>
+                    <p>
+                       <h3>Banned Hacks</h3>
+                    </p>
+                    <p>
+                       - Noclip
+                    </p>
+                    <p>
+                       - Speedhack below x1 Speed
+                    </p>
+                    <p>
+                       - Show Hitboxes
+                    </p>
+                    <p>
+                       - Show Layout
+                    </p>
+                    <p>
+                       - Replay Bot / Macro
+                    </p>
+                    <p>
+                       - No Mirror
+                    </p>
+                    <p>
+                       - Hitbox Multiplier
+                    </p>
                 </div>
             </div>
         </main>
