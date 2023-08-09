6ca428f863c616da959aff3e51ace92f2e43c57b
Simon <simon.giesse@gmx.de>
2023-08-09 02:38:39 +0200

    Removed duplicates and added duplicate finder

---
+++ findDuplicates.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+def fixData():
+    current_dir = os.path.join(os.getcwd(), "data")
+    list_path = os.path.join(current_dir, "_list.json")
+    levels = []
+    with open(list_path, "r") as file:
+        levels = json.load(file)
+        
+    for filename in levels:
+        if filename.startswith("_"):
+                continue
+        file_path = os.path.join(current_dir, f"{filename}.json")
+        lines = []
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            records = data["records"]
+            names = []
+            for record in records:
+                name = record["user"]
+                if name in names:
+                    print(f"Duplicate {filename}: {name}")
+                    
+                names.append(name)
+
+if __name__ == "__main__":
+    fixData()
