aeab7bcdc20388c7209e4d95378a73a0b23366ee
KrisGra <39800337+krisgrant@users.noreply.github.com>
2023-09-23 23:59:34 +0300

    getting rid of all my code

--- js/content.js
+++ js/content.js
@@ -25,7 +25,7 @@ export async function fetchList() {
                             packs,
                             path,
                             records: level.records.sort(
-                                (a, b) => b.percent - a.percent
+                                (a, b) => b.percent - a.percent,
                             ),
                         },
                         null,
@@ -57,7 +57,6 @@ export async function fetchLeaderboard() {
     const packResult = await (await fetch(`${dir}/_packlist.json`)).json();
     const scoreMap = {};
     const errs = [];
-    const packMultiplier = 1.5;
     list.forEach(([level, err], rank) => {
         if (err) {
             errs.push(err);
@@ -73,7 +72,7 @@ export async function fetchLeaderboard() {
             verified: [],
             completed: [],
             progressed: [],
-            packs: [],
+            packs: []
         };
         const { verified } = scoreMap[verifier];
         verified.push({
@@ -81,7 +80,7 @@ export async function fetchLeaderboard() {
             level: level.name,
             score: score(rank + 1, 100, level.percentToQualify),
             link: level.verification,
-            path: level.path,
+            path: level.path
         });
         // Records
         level.records.forEach((record) => {
@@ -93,7 +92,7 @@ export async function fetchLeaderboard() {
                 verified: [],
                 completed: [],
                 progressed: [],
-                packs: [],
+                packs: []
             };
 
             const { completed, progressed } = scoreMap[user];
@@ -103,7 +102,7 @@ export async function fetchLeaderboard() {
                     level: level.name,
                     score: score(rank + 1, 100, level.percentToQualify),
                     link: record.link,
-                    path: level.path,
+                    path: level.path
                 });
                 return;
             }
@@ -114,17 +113,15 @@ export async function fetchLeaderboard() {
                 percent: record.percent,
                 score: score(rank + 1, record.percent, level.percentToQualify),
                 link: record.link,
-                path: level.path,
+                path: level.path
             });
         });
     });
-    for (let user of Object.entries(scoreMap)) {
-        let levels = [...user[1]["verified"], ...user[1]["completed"]].map(
-            (x) => x["path"]
-        );
-        for (let pack of packResult) {
-            if (pack.levels.every((e1) => levels.includes(e1))) {
-                user[1]["packs"].push(pack);
+    for (let user of Object.values(scoreMap)){
+        let levels = ([...user["verified"], ...user["completed"]]).map((x)=>x["path"]);
+        for (let pack of packResult){
+            if (pack.levels.every(e1=>levels.includes(e1))){
+                user["packs"].push(pack);
             }
         }
         // for (let pack of user[1]["packs"]) {
@@ -146,75 +143,17 @@ export async function fetchLeaderboard() {
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
         const { verified, completed, progressed } = scores;
-
-        let packScore = 0;
-        let packScoreMultiplied = 0;
-        for (let pack of scores["packs"]) {
-            const packLevelScores = [];
-            const allUserLevels = [
-                ...scores["verified"],
-                ...scores["completed"],
-            ];
-            for (let level of pack["levels"]) {
-                let userLevel = allUserLevels.find((lvl) => lvl.path == level);
-                packLevelScores.push(userLevel.score);
-            }
-            packLevelScores.forEach((score) => (packScore += score));
-            packScoreMultiplied = packScore * packMultiplier;
-        }
-
-        let totalWithoutBonus = [verified, completed, progressed]
+        const total = [verified, completed, progressed]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
-        const total = totalWithoutBonus - packScore + packScoreMultiplied
 
         return {
             user,
             total: round(total),
-            packBonus: round(total - totalWithoutBonus),
             ...scores,
         };
     });
 
     // Sort by total score
     return [res.sort((a, b) => b.total - a.total), errs];
-}
-
-export async function fetchPacks() {
-    try {
-        const packResult = await fetch(`${dir}/_packlist.json`);
-        const packsList = await packResult.json();
-        return packsList;
-    } catch {
-        return null;
-    }
-}
-
-export async function fetchPackLevels(packname) {
-    const packResult = await fetch(`${dir}/_packlist.json`);
-    const packsList = await packResult.json();
-    const selectedPack = await packsList.find((pack) => pack.name == packname);
-    try {
-        return await Promise.all(
-            selectedPack.levels.map(async (path, rank) => {
-                const levelResult = await fetch(`${dir}/${path}.json`);
-                try {
-                    const level = await levelResult.json();
-                    return [
-                        {
-                            level,
-                            path,
-                        },
-                        null,
-                    ];
-                } catch {
-                    console.error(`Failed to load level #${rank + 1} ${path}.`);
-                    return [null, path];
-                }
-            })
-        );
-    } catch (e) {
-        console.error(`Failed to load packs.`, e);
-        return null;
-    }
 }
