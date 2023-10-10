4ae672ced3cd0f3331abf2f034ff5678fa186e47
Simon <simon.giesse@gmx.de>
2023-10-10 21:20:37 +0200

    fixed some creators

--- validateData.py
+++ validateData.py
@@ -29,44 +29,52 @@ def validateData():
     for filename in levels:
         file_path = os.path.join(current_dir, f"{filename}.json")
         lines = []
-        with open(file_path, "r") as file:
-            data = []
-            try:
-                data = json.load(file)
-            except ValueError as e:
-                print(f"Invalid json in file {filename}: {str(e)}")
-                had_error = True
-                continue
-                
-            records = data["records"]
-            names = []
-            try:
-                validator(data["verification"])
-            except ValidationError:
-                had_error = True
-                print(f"Invalid verification Url: {filename}: {url}")
-                    
-            for record in records:
-                
-                name = record["user"].lower()
-                if name in names:
-                    had_error = True
-                    print(f"Duplicate Record: {filename}: {name}")
-                    
-                names.append(name)
-                url = record["link"]
+        try:
+            with open(file_path, "r") as file:
+                data = []
                 try:
-                    validator(url)
+                    data = json.load(file)
+                except ValueError as e:
+                    print(f"Invalid json in file {filename}: {str(e)}")
+                    had_error = True
+                    continue
+                
+                records = data["records"]
+                names = []
+                try:
+                    validator(data["verification"])
                 except ValidationError:
                     had_error = True
-                    print(f"Invalid Url: {filename} {name}: {url}")
+                    print(f"Invalid verification Url: {filename}: {url}")
                     
-            creators = []
-            for creator in data["creators"]:
-                if creator in creators:
-                    had_error = True
-                    print(f"Duplicate Creator: {filename}: {creator}")
-                creators.append(creator)
+                for record in records:
+                
+                    name = record["user"].lower()
+                    if name in names:
+                        had_error = True
+                        print(f"Duplicate Record: {filename}: {name}")
+                    
+                    names.append(name)
+                    url = record["link"]
+                    try:
+                        validator(url)
+                    except ValidationError:
+                        had_error = True
+                        print(f"Invalid Url: {filename} {name}: {url}")
+                    
+                creators = []
+                for creator in data["creators"]:
+                    if creator in creators:
+                        had_error = True
+                        print(f"Duplicate Creator: {filename}: {creator}")
+                    
+                    if "," in creator:
+                        had_error = True
+                        print(f"Invalid Creator: {filename}: {creator}")
+                    creators.append(creator)
+        except FileNotFoundError:
+            had_error = True
+            print(f"Missing file {filename}")
                 
     if had_error:
         sys.exit(1)
