7014b808e9f038638cb1b2aa2a2815bfbdc8a301
Simon <simon.giesse@gmx.de>
2024-03-16 00:12:58 +0100

    New guidelines

--- src/views/List.vue
+++ src/views/List.vue
@@ -20,16 +20,66 @@ const selected_level = ref()
       <div class="info-view">
         <ListStaff></ListStaff>
         <div class="info-text">
-          <h3>
-            > How to Submit Records
-          </h3>
-          <div class="info-paragraph">
-            <p>
-              Log into the site with discord. <br>
-              Hover over your profile avatar. <br>
-              You'll find the submission page in the dropdown menu.
-            </p>
-          </div>
+          <h3>> How to Submit Records</h3>
+          <p>
+            Join our discord server, and  use /record submit
+          </p>
+          <h3>> Submission Requirements</h3>
+          <p>
+            When submitting your record, please ensure that it complies with the following guidelines:
+          </p>
+          <p>
+            - Your recording is a complete playthrough of the level from 0-100 without any cuts (if there are cuts in your video, please include an uncut raw footage)
+          </p>
+          <p>
+            - Your completion needs to have clicks. If it doesn't (or if it only does for a part of the level, and not the entire run), you have to provide a raw footage with clicks. If you don't, your record will be rejected. Mobile players are NOT exempt from this rule, recorded taps are required.
+          </p>
+          <p>
+            - Achieved the record on the level that is listed on the site (or an approved LDM of it) - please check the level ID before you submit a record
+          </p>
+          <p>
+            - The record has been achieved without using a secret way or a bugged route
+          </p>
+          <p>
+            - Cheat Indicator is required if you are using a mod menu that supports one, like Megahack v8.
+          </p>
+          <p>
+            - End stats (The whole box must appear for at least one frame)
+          </p>
+          <p>
+            - FPS/TPS Counter (For mod menus that support one, or the 2.2 native FPS counter otherwise)
+          </p>
+          <p>
+            - Using physics bypass on any level in 2.2 is not allowed, and will get your record rejected
+          </p>
+          <p>
+            - Using the 2.1 GDPS is allowed for levels verified in 2.1, up to 360 FPS/TPS
+          </p>
+          <p>
+            - Raw footage (or a pointercrate record of the level) AND clicks will ALWAYS be required for levels in the top 250.
+          </p>
+          <p>
+            - Refer to <a href="https://docs.google.com/spreadsheets/d/10AjUUcTpAzOqdfnbz0wtYskL9TdwT4Onv_Fldt5NXBg/edit?usp=sharing">-This Sheet-</a> for a list of allowed and disallowed hacks
+          </p>
+          <h3>> Why was my record denied?</h3>
+          <p>
+            If your record was denied, please check the following:
+          </p>
+          <p>
+            Does the video meet the requirements? (Above)
+          </p>
+          <p>
+            Is the level placed on the list? (#pending-changes)
+          </p>
+          <p>
+            Was the submission command filled out correctly?
+          </p>
+          <p>
+            Was the record submitted with several links?
+          </p>
+          <p>
+            Note: You will be pinged in #records with the reason why your record was denied. If you are still unsure of why it was denied, or if the record was wrongfully denied, please make a post in #support or DM any list staff on Discord
+          </p>
         </div>
       </div>
     </div>
