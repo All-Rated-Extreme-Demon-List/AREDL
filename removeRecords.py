5e935c6df96652da0397e8b48ba8d9f61e1bf235
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2024-09-22 09:29:01 +0000

    Changed remove script to use IDs

--- removeRecords.py
+++ removeRecords.py
@@ -2,14 +2,41 @@ import os
 import sys
 import json
 
-def remove_records(user):
-    print(f"Removing records for user: {user}")
+def remove_records(user_name):
+    
 
     nb_removed = 0
 
     current_dir = os.path.join(os.getcwd(), "data")
     list_path = os.path.join(current_dir, "_list.json")
     legacy_list_path = os.path.join(current_dir, "_legacy.json")
+    name_map_path = os.path.join(current_dir, "_name_map.json")
+
+    print(f"Looking up ID for user: {user_name}")
+    try:
+        with open(name_map_path, "r", encoding='utf-8') as file:
+            name_map = json.load(file)
+    except ValueError as e:
+        print(f"Invalid json in file _name_map.json: {str(e)}")
+        sys.exit(1)
+
+    user_id = None
+    for uid, uname in name_map.items():
+        if uname == user_name:
+            user_id = uid
+            break
+    
+    if not user_id:
+        print(f"User {user_name} not found in _name_map.json.")
+        sys.exit(1)
+
+    try:
+        user_id = int(user_id)
+    except ValueError as e:
+        print(f"Invalid user ID {user_id} for user {user_name}: {str(e)}")
+        sys.exit(1)
+
+    print(f"Removing records for user ID: {user_id}")
 
     try:
         with open(list_path, "r", encoding='utf-8') as file:
@@ -36,7 +63,7 @@ def remove_records(user):
             sys.exit(1)
 
         original_records = data['records'][:]
-        data['records'] = [record for record in data['records'] if record['user'] != user]
+        data['records'] = [record for record in data['records'] if record['user'] != user_id]
 
         if len(data['records']) < len(original_records):
             nb_removed += len(original_records) - len(data['records'])
@@ -47,7 +74,7 @@ def remove_records(user):
             except ValueError as e:
                 print(f"Failed to write json to file {filename}.json: {str(e)}")
 
-    print(f"Successfully removed {nb_removed} records for user {user}")
+    print(f"Successfully removed {nb_removed} records for user {user_name}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
