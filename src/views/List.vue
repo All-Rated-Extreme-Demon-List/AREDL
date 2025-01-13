562d1bad64a97c73aa9eef77d2f59b3f01c0eeef
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2025-01-13 12:51:20 +0100

    Update List.vue

--- src/views/List.vue
+++ src/views/List.vue
@@ -24,11 +24,11 @@ const mobile_list_expanded = ref(true)
       <div class="info-view">
         <ListStaff></ListStaff>
         <div class="info-text">
-          <h3>> How to Submit Records</h3>
+          <h3> How to Submit Records</h3>
           <p>
-            Join our <a href="https://discord.gg/aredl">discord server</a>, and  use /record submit
+            Join our <a href="https://discord.gg/aredl">discord server</a>, and use /record submit. You can find more information about submitting records in the #record-submissions channel
           </p>
-          <h3>> Submission Requirements</h3>
+          <h3>Submission Requirements</h3>
           <p>
             When submitting your record, please ensure that it complies with the following guidelines:
           </p>
@@ -36,24 +36,26 @@ const mobile_list_expanded = ref(true)
             1. Your record must have clicks, COMPLETELY audible throughout the whole level. Not audible sometimes, at the end, or the beginning the whole level. "But what if there was a mistake with the-" We apologize, but we have to be able to hear full audible clicks throughout your video. (Taps, if you're playing on mobile. Same rules apply.)
           </p>
           <p>
-            2. Your record must have a cheat indicator either overlaid in your level, or located on the end screen. However, there are exceptions to this rule. If you're playing vanilla GD, or you have a mod menu that does not use a cheat indicator/older version of mega hack. Megahack users are required to have the drop-down endscreen cheat indicator enabled.However, you have to put that in your notes, we are not responsible for figuring out which mod menu you used, or if you're playing on vanilla.
-          </p>
+            2. Your record must have a cheat indicator on the end screen. If you're playing vanilla GD, or you have a mod menu that does not use a cheat indicator, this is not required. However, you have to put that in your notes, we are not responsible for figuring out which mod menu you used, or if you're playing on vanilla. </p>
           <p>
-            3. If you are using an LDM, or a bugfix copy of a level, it must either be approved by list staff, or obviously fine. Use your best judgement here, if you are uncertain whether or not your bugfix copy/LDM of a level can be accepted, file a support ticket with the level ID. However, if you're absolutely certain that your copy is fine, you don't need to get it approved in order to get your record accepted. If you're unaware of what an "acceptable" copy would be, go ahead and file a support ticket. (We deny copies that either change gameplay of the level, or remove so much detail that it makes the level easier.
-          </p>
+            3. If you are using an LDM or a bugfix copy of a level, it must either be approved by list staff, or not make a difference without a doubt. Use your best judgement here, if you are uncertain whether or not your bugfix copy/LDM of a level can be accepted, file a support ticket with the level ID. However, if you're absolutely certain that your copy is fine, you don't need to get it approved in order to get your record accepted. If you're unaware of what an "acceptable" copy would be, go ahead and file a support ticket. (We deny copies that either change gameplay of the level, or remove so much detail that it makes the level easier.) </p>
           <p>
             4. Your record must have an uncut end screen. If your video ends before we see the end screen, and your stats are not visible, it is not an acceptable record.
           </p>
           <p>
-            5. If your record is found in the top 400, you must have raw footage submitted along with your original record in a downloadable format, for example: Google Drive, no matter what. However, if you have the record on your Pointercrate profile, you may submit that in the additional info instead, and we will accept your record. However, please note that we will not accept records that used Physics Bypass while it was allowed on pointercrate, since Physics Bypass was not ever allowed on the AREDL.
-          </p>
+            5. If your record is in the top 400, you must have raw footage, with split audio tracks, submitted along with your original record in a downloadable format such as Google Drive. If it was streamed, a VOD on Twitch or Youtube with chat enabled is also allowed. However, if you have the record on your Pointercrate profile, you may submit that in the additional info instead, and we will accept your record. However, please note that we will not accept records that used Physics Bypass while it was allowed on pointercrate, since Physics Bypass was not ever allowed on the AREDL.<br/>
+            If you need help setting up split audio tracks, try following one of these guides:
+            <ul>
+              <li><a href="https://obsproject.com/kb/multiple-audio-track-recording-guide">OBS</a></li>
+              <li><a href="https://support.medal.tv/support/solutions/articles/48001220541-multi-track-recording">Medal</a></li>
+            </ul>
+            </p>
           <p>
             6. This should be obvious, but your record may not be completed with any disallowed mods. This information (of what we allow and don't allow) can be found on our website. This rule also applies to having a red/blue cheat indicator, obvious hacked completions, and bots.
           </p>
           <p>
-            7. Globed 2P Completions will be allowed on AREDL, however, your video need to have both POVs, or you need to submit your friends video as "raw footage". Globed deathlink will not be allowed, since Globed 2P level completions must have dual physics.
-          </p>
-          <h3>> Why was my record denied?</h3>
+            7. Globed 2P Completions will be allowed on AREDL, however, your video needs to have both POVs, or you need to submit your friend's video in the notes. Globed deathlink is allowed for levels that do not work with the built in Globed 2P mode, but these completions needs to be on an official AREDL Globed copy, which can be found on the "AREDLofficial" Geometry Dash account.</p>
+          <h3> Why was my record denied?</h3>
           <p>
             If your record was denied, please check the following:
           </p>
